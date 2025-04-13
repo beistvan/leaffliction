694c2760850920bd5b2512f48dc6bdb211601f1c
Vitalik <frantsv2004@gmail.com>
2025-04-13 17:55:30 +0200

    chore: init structure

---
+++ src/model/predict.py
@@ -0,0 +1,11 @@
+
+
+def main() -> None:
+    """
+    Main function to predict using the model.
+    """
+    print("Predicting using the model...")
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/model/train.py
@@ -0,0 +1,11 @@
+
+
+def main() -> None:
+    """
+    Main function to train the model.
+    """
+    print("Training the model...")
+
+
+if __name__ == '__main__':
+    main()
